62edc51d1258818c667125427099a610dc34c3dc
MiguelMagalhaes1205 <miguel.magalhaes@sptech.school>
2026-03-10 14:34:46 -0300

    Mudança no codigo

--- src/controllers/recipes/getRecipe.ts
+++ src/controllers/recipes/getRecipe.ts
@@ -23,7 +23,8 @@ export async function getRecipe(req, res) {
         "1/2 tsp paprika",
         "1/4 tsp red pepper flakes",
         "1/4 cup parsley",
-        "8 oz fettuccine pasta"
+        "8 oz fettuccine pasta",
+        "1 tbsp olive oil"
       ],
       cookingTime: 30,
       instructions: [
